58ff8a009ec7c1418e567079c575d96fa400a868
Ryuunotora <144704234+Ryuunotora@users.noreply.github.com>
2024-07-02 13:49:59 -0500

    Initial commit

---
+++ src/main/java/pe/edu/cibertec/DSWII_T3_AbelPerez/DswiiT3AbelPerezApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_T3_AbelPerez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3AbelPerezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3AbelPerezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWII_T3_AbelPerez/DswiiT3AbelPerezApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_T3_AbelPerez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiT3AbelPerezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
