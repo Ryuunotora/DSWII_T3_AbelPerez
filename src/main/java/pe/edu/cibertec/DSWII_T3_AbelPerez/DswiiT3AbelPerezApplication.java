24c3c04a52e46bdbedfbaf68010cdd641b54a7a4
Ryuunotora <144704234+Ryuunotora@users.noreply.github.com>
2024-07-02 22:29:57 -0500

    GlobalCors Listo

--- src/main/java/pe/edu/cibertec/DSWII_T3_AbelPerez/DswiiT3AbelPerezApplication.java
+++ src/main/java/pe/edu/cibertec/DSWII_T3_AbelPerez/DswiiT3AbelPerezApplication.java
@@ -2,6 +2,9 @@ package pe.edu.cibertec.DSWII_T3_AbelPerez;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DswiiT3AbelPerezApplication {
@@ -9,5 +12,19 @@ public class DswiiT3AbelPerezApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DswiiT3AbelPerezApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer configGlobalCors(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("api/v1/**")
+						.allowedOrigins("https://www.cibertec.edu.pe")
+						.allowedMethods("GET", "POST", "PUT")
+						.allowedHeaders("*");
+			}
+		};
+	}
+
 
 }
